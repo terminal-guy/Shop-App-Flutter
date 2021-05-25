1932c3d1f126492ea4ce2936177a1b265146c11c
terminal-guy <vishaladhithya19@gmail.com>
2021-05-25 19:46:46 +0530

    adding some productitem images to the main screen

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../models/product.dart';
+import '../widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = [
@@ -39,6 +40,27 @@ class ProductsOverviewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10.0),
+        itemCount: loadedProducts.length,
+        itemBuilder: (context, i) {
+          return ProductItem(
+            id: loadedProducts[i].id,
+            title: loadedProducts[i].title,
+            imageUrl: loadedProducts[i].imageUrl,
+          );
+        },
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10, // row spacing.
+        ),
+      ),
+    );
   }
 }
