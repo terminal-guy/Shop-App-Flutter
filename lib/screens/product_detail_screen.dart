6ea80fdfa7278292cd7d10757238ef8ee23bf668
terminal-guy <vishaladhithya19@gmail.com>
2021-05-26 13:23:30 +0530

    added roues const and modalroute

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final String price;
+  // ProductDetailScreen(this.title, this.price);
+
+  static const screenName = '/product-detail';
+
   @override
   Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text('title'),
+      ),
     );
   }
 }
