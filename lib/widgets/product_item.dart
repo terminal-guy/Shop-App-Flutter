e144e1370af7f2e7dbdbd6d36e0a2b25f83e4990
terminal-guy <vishaladhithya19@gmail.com>
2021-05-27 09:06:00 +0530

    adding notes :package:

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -13,6 +13,8 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context);
+// * NOTES
+// this checks for the Product ChangeNotiferprovider from widget to the top most widget using Product.
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -38,6 +40,7 @@ class ProductItem extends StatelessWidget {
                 product.isFavorite ? Icons.favorite : Icons.favorite_border),
             onPressed: () {
               product.toggleFavoriteStatus();
+// provider method for toggling favorites
             },
             color: Theme.of(context).accentColor,
           ),
