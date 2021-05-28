bbb87bbc6d9e3e7980743e86f5839025596d679f
terminal-guy <vishaladhithya19@gmail.com>
2021-05-28 19:00:13 +0530

    adding consumer :tada:

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,9 +12,10 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
 // * NOTES
 // this checks for the Product ChangeNotiferprovider from widget to the top most widget using Product.
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -35,14 +36,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(
-                product.isFavorite ? Icons.favorite : Icons.favorite_border),
-            onPressed: () {
-              product.toggleFavoriteStatus();
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavoriteStatus();
 // provider method for toggling favorites
-            },
-            color: Theme.of(context).accentColor,
+              },
+              color: Theme.of(context).accentColor,
+            ),
           ),
           title: Text(
             product.title,
