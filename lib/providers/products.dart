584b25b54ed8a7eaa10079b5181913690926f7fb
terminal-guy <vishaladhithya19@gmail.com>
2021-05-28 19:37:27 +0530

    adding getter for getting favorite items

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -36,8 +36,13 @@ class Products with ChangeNotifier {
           'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
+
   List<Product> get items {
     return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   Product findById(String id) {
