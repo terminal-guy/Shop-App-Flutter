80917150b79629047478705abcddcf1dc0463f63
terminal-guy <vishaladhithya19@gmail.com>
2021-06-09 14:03:37 +0530

    delete products

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/products.dart';
 
 import 'package:shop_app/screens/edit_product_screen.dart';
 
@@ -31,7 +33,9 @@ class UserProductItem extends StatelessWidget {
               ),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               icon: Icon(
                 Icons.delete,
                 color: Theme.of(context).errorColor,
